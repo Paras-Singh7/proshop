c1caf987c877d0dbfbfded24cb842c8ccb98861b
Paras Singh <sparas348@gmail.com>
2024-06-08 17:12:53 +0530

    build added

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vuq&au#n(#d0c&hg(m51)5c+jf-*n6*!fh^nl#mi-c$87)iom5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
